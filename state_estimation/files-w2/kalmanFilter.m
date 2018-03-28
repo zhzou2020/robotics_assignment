e5b1329197e200d0b818a19abda1026f87a87ef9
zzouac <zzouac@ust.hk@ecelvd185.ece.local>
2018-03-28 18:47:17 +0800

    implement kalman filter

--- state_estimation/files-w2/kalmanFilter.m
+++ state_estimation/files-w2/kalmanFilter.m
@@ -3,26 +3,40 @@ function [ predictx, predicty, state, param ] = kalmanFilter( t, x, y, state, pa
 %   Four dimensional state: position_x, position_y, velocity_x, velocity_y
 
     %% Place parameters like covarainces, etc. here:
-    % P = eye(4)
-    % R = eye(2)
+    dt = t - previous_t;
+    
+    F = [1 0 dt 0;
+         0 1 0 dt;
+         0 0 1 0;
+         0 0 0 1];
+    Q = 0.1 * eye(4);
+    H = [1 0 0 0;
+         0 1 0 0];
+    R = 0.01 * eye(2);
 
-    % Check if the first time running this function
+    % Check if the first time running this function 
     if previous_t<0
         state = [x, y, 0, 0];
-        param.P = 0.1 * eye(4);
         predictx = x;
         predicty = y;
+        param.P = 1 * eye(4);
         return;
     end
 
-    %% TODO: Add Kalman filter updates
-    % As an example, here is a Naive estimate without a Kalman filter
-    % You should replace this code
-    vx = (x - state(1)) / (t - previous_t);
-    vy = (y - state(2)) / (t - previous_t);
-    % Predict 330ms into the future
-    predictx = x + vx * 0.330;
-    predicty = y + vy * 0.330;
-    % State is a four dimensional element
-    state = [x, y, vx, vy];
+    state = state.';
+    state =  F * state;
+    P = F * param.P * F.' + Q;
+    
+    y = [x;y] - H * state;
+    S = H * P * H.' + R;
+    K = P * H.' / S;
+    state = state + K * y;
+    P = (eye(4) - K * H) * P;
+    
+    param.P = P;
+    
+    state = state.';
+    
+    predictx = state(1) + 0.33 * state(3);
+    predicty = state(2) + 0.33 * state(4);
 end
